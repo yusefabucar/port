38e12f6185dfa1d72ad9b6f0fe59db3972ed0e71
Yusef Abucar <92749899+yusefabucar@users.noreply.github.com>
2023-03-15 17:30:43 -0400

    more

--- src/components/About.js
+++ src/components/About.js
@@ -34,28 +34,26 @@ const About = () => {
           >
             <h2 className='h2 text-accent'>About me.</h2>
             <h3 className='h3 mb-4'>
-              I'm a Freelance Front-end Developer with over 5 years of
-              experience.
+              Freelance Graphic Artist, 3D Modeler, and Software Developer.
             </h3>
             <p className='mb-6'>
-              Lorem ipsum dolor sit, amet consectetur adipisicing elit. Dolor
-              natus omnis inventore quas beatae illo nulla. Officiis magni omnis
-              molestias.
+              I'm a visual artist and professional web developer: When modern web development meets traditional art and design. Basically, the best of both worlds!
             </p>
             {/* stats */}
             <div className='flex gap-x-6 lg:gap-x-10 mb-12'>
               <div>
                 <div className='text-[40px] font-tertiary text-gradient mb-2'>
-                  {inView ? <CountUp start={0} end={13} duration={3} /> : null}
+                  {inView ? <CountUp start={0} end={10} duration={3} /> : null}
+                  +
                 </div>
                 <div className='font-primary text-sm tracking-[2px]'>
-                  Years of <br />
-                  Experience
+                  Specilized <br />
+                  fields of study
                 </div>
               </div>
               <div>
                 <div className='text-[40px] font-tertiary text-gradient mb-2'>
-                  {inView ? <CountUp start={0} end={15} duration={3} /> : null}
+                  {inView ? <CountUp start={0} end={11} duration={3} /> : null}
                   k+
                 </div>
                 <div className='font-primary text-sm tracking-[2px]'>
@@ -65,12 +63,12 @@ const About = () => {
               </div>
               <div>
                 <div className='text-[40px] font-tertiary text-gradient mb-2'>
-                  {inView ? <CountUp start={0} end={12} duration={3} /> : null}
-                  k+
+                  {inView ? <CountUp start={0} end={25} duration={3} /> : null}
+                  +
                 </div>
                 <div className='font-primary text-sm tracking-[2px]'>
-                  Satisfied <br />
-                  Clients
+                  Years of <br />
+                  Experience
                 </div>
               </div>
             </div>
